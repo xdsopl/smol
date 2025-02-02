6709f9a0bc7a9a2b5ed9071af9a3550d0740ff79
Ahmet Inan <xdsopl@gmail.com>
2025-02-02 22:07:47 +0100

    use adaptive Rice coding

--- vli.h
+++ vli.h
@@ -9,38 +9,37 @@ Copyright 2025 Ahmet Inan <xdsopl@gmail.com>
 #include "bits.h"
 
 int putval(int val) {
-	int cnt = 0, top = 1;
-	while (top <= val) {
-		cnt += 1;
-		top = 1 << cnt;
-		if (putbit(1))
+	static int order;
+	while (val >= 1 << order) {
+		if (putbit(0))
 			return -1;
+		val -= 1 << order;
+		order += 1;
 	}
-	if (putbit(0))
+	if (putbit(1))
 		return -1;
-	if (cnt > 0) {
-		cnt -= 1;
-		val -= top / 2;
-		if (write_bits(val, cnt))
-			return -1;
-	}
+	if (write_bits(val, order))
+		return -1;
+	order -= 1;
+	if (order < 0)
+		order = 0;
 	return 0;
 }
 
 int getval() {
-	int val = 0, cnt = 0, top = 1, ret;
-	while ((ret = getbit()) == 1) {
-		cnt += 1;
-		top = 1 << cnt;
+	static int order;
+	int val, sum = 0, ret;
+	while ((ret = getbit()) == 0) {
+		sum += 1 << order;
+		order += 1;
 	}
 	if (ret < 0)
 		return -1;
-	if (cnt > 0) {
-		cnt -= 1;
-		if (read_bits(&val, cnt))
-			return -1;
-		val += top / 2;
-	}
-	return val;
+	if (read_bits(&val, order))
+		return -1;
+	order -= 1;
+	if (order < 0)
+		order = 0;
+	return val + sum;
 }
 
