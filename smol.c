53eadc67a0554911d9556a4f3c834c8b2a91582d
Ahmet Inan <xdsopl@gmail.com>
2025-02-02 18:08:54 +0100

    fill rest of table with printable chars first

--- smol.c
+++ smol.c
@@ -5,10 +5,21 @@ Copyright 2025 Ahmet Inan <xdsopl@gmail.com>
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "table.h"
 
 int main() {
-	for (unsigned char *p = table; *p; ++p)
+	unsigned char *p;
+	for (p = table; *p; ++p)
 		printf("%2x: [%c]\n", *p, *p);
+	printf("--\n");
+	for (int i = 32; i < 127; ++i)
+		if (!strchr((char *)table, i))
+			printf("%2x: [%c]\n", *p++ = i, i);
+	for (int i = 127; i < 256; ++i)
+		*p++ = i;
+	for (int i = 0; i < 32; ++i)
+		*p++ = i;
+	printf("count = %ld\n", p - table);
 	return 0;
 }
