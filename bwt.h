db826907021ecf4f70a0082152dfa30d0f870ca7
Ahmet Inan <xdsopl@gmail.com>
2025-02-04 22:13:29 +0100

    only sort the suffixes

--- bwt.h
+++ bwt.h
@@ -18,16 +18,13 @@ static const unsigned char *bwt_input;
 int bwt_compare(const void *a, const void *b) {
 	int x = *(const int *)a;
 	int y = *(const int *)b;
-	for (int i = 0; i < bwt_length; ++i) {
-		int l = bwt_input[(x + i) % bwt_length];
-		int r = bwt_input[(y + i) % bwt_length];
-		if (l != r)
-			return l - r;
-	}
-	return 0;
+	for (; x < bwt_length && y < bwt_length; ++x, ++y)
+		if (bwt_input[x] != bwt_input[y])
+			return bwt_input[x] - bwt_input[y];
+	return y - x;
 }
 
-void bwt_rot(int *output, const unsigned char *input, int length) {
+void bwt_sa(int *output, const unsigned char *input, int length) {
 	bwt_input = input;
 	bwt_length = length;
 	for (int i = 0; i < length; ++i)
@@ -41,10 +38,10 @@ int bwt(unsigned char *output, const unsigned char *input, int length) {
 			return 1;
 	if (input[length-1])
 		return 1;
-	static int rot[BLOCK_SIZE];
-	bwt_rot(rot, input, length);
+	static int sa[BLOCK_SIZE];
+	bwt_sa(sa, input, length);
 	for (int i = 0; i < length; ++i) {
-		int index = rot[i];
+		int index = sa[i];
 		if (index == 0)
 			index = length;
 		output[i] = input[index - 1];
